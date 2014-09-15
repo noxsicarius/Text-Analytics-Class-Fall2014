1739e3cdbc4a6f94e1e148e0a0d4aa7879f9701d
noxsicarius <sanderrick2000@yahoo.com>
2014-09-15 19:38:18 -0400

    Updated link search
    The link search now excludes files to only obtain the links by deleting
    the file portion, loading everything into a temp file, and then running
    the link search.

--- HW2/xmlLinkSearch.java
+++ HW2/xmlLinkSearch.java
@@ -1,6 +1,10 @@
 
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
 import java.nio.channels.FileChannel;
@@ -10,20 +14,38 @@ import java.util.regex.Pattern;
 
 public class xmlLinkSearch {
     public static void main(String[] args) throws IOException {
-       Pattern categoryTag = Pattern.compile("\\[\\[.*?\\]\\]");
+       Pattern filePattern = Pattern.compile("\\[\\[File.*?\\[\\[");
+       Pattern linkPattern = Pattern.compile("\\[\\[.*?\\]\\]");
        String filePath = System.getProperty("user.home") + "/Documents/GitHub/Text-Analytics-Class-Fall2014/HW2";
        FileInputStream input = new FileInputStream(filePath + "/simplewiki-20140814-pages-articles-multistream.xml");
+       Writer tempFile = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(filePath + "/tempFile.xml"), "utf-8"));
+       Writer outputFile = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(filePath + "/querryLinks.txt"), "utf-8"));
        FileChannel channel = input.getChannel();
-          
        ByteBuffer bbuf = channel.map(FileChannel.MapMode.READ_ONLY, 0, (int) channel.size());
        CharBuffer cbuf = Charset.forName("8859_1").newDecoder().decode(bbuf);
-       Matcher tagmatch = categoryTag.matcher(cbuf);
-
-       while (tagmatch.find()){
-           String match = tagmatch.group();
+       Matcher fileMatch = filePattern.matcher(cbuf);
+       String bufferString = cbuf.toString();
+       
+       while (fileMatch.find()){
+           String match = fileMatch.group();
+           bufferString = bufferString.replace(match,"[[");
+           //System.out.println(output);
+           
+       }
+       tempFile.write(bufferString);
+       
+       input = new FileInputStream(filePath + "/tempFile.xml");
+       channel = input.getChannel();
+       bbuf = channel.map(FileChannel.MapMode.READ_ONLY, 0, (int) channel.size());
+       cbuf = Charset.forName("8859_1").newDecoder().decode(bbuf);
+       Matcher linkMatch = linkPattern.matcher(cbuf);
+       while (linkMatch.find()){
+           String match = linkMatch.group();
            match = match.replaceAll("\\[","");
            match = match.replaceAll("\\]","");
-           System.out.println("\"" + match + "\"");
+           String output = "\"" + match + "\"";
+           System.out.println(output);
+           outputFile.write(output + "\n");
        }
-    }    
+    }
 }
